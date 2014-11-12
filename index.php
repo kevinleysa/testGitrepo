28b5e14b1a0e54b02bcc97a08b2e724723abfee2
Kevin Leysa <kevinleysa@gmail.com>
2014-11-12 11:29:33 +0800

    Add index.php file

---
+++ index.php
@@ -0,0 +1,18 @@
+<html><body>
+<?php echo phpinfo(); ?>
+<?php
+
+
+	echo"<center><b><h1>Welcome to Linspira</h1></b></center>";
+	
+	echo"<h2><br><br>More about 'Linspira' = Linked + Inspirations</h2>";
+	echo"<center><div style=color:Red>
+	<h2><br><br>Your idea CAN change the WORLD!</h2></div>
+	<br><br>
+
+	</center>
+	";
+	
+?>
+
+</body></html>
